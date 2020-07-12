2aed60e94648dfdfead02f23bf61db5fbd6be729
SAJNIN123 <62798104+SAJNIN123@users.noreply.github.com>
2020-07-12 14:24:04 +0600

    file upload

---
+++ Js/script.js
@@ -0,0 +1,30 @@
+$(function () {
+    var menuTopOffSet = $('.menu').offset().top
+    $(window).on('scroll',function () {
+       var scrollSize = $(window).scrollTop()
+
+       if(scrollSize > menuTopOffSet){
+           $('.menu').addClass('menuFixed')
+       }else{
+        $('.menu').removeClass('menuFixed') 
+       }
+
+  
+
+       if(scrollSize > 800){
+        $('#backToTop').fadeIn(1000)
+    }else{
+        $('#backToTop').fadeOut(1000)
+    }
+
+})
+
+    $('#backTop').on('click',function () {
+        $('html,body').animate({
+            scrollTop:0
+        },1000)
+   
+    })
+
+
+})
