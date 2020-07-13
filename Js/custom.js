59e5386d563ff89bfbe6d54cd6ca88603c17f008
SAJNIN123 <62798104+SAJNIN123@users.noreply.github.com>
2020-07-13 14:25:42 +0600

    fhhjj

--- Js/custom.js
+++ Js/custom.js
@@ -1,3 +1,19 @@
 $(function () {
-    $('#banner').slick();
+    //Banner Slider
+    $('#banner').slick({
+        arrows: false,
+        autoplay:true,
+        autoplaySpeed: 1000,
+        speed:500,
+       
+    });
+
+    //About Slider
+    $('.about_slider').slick({
+        infinite: true,
+        slidesToShow:4,
+        slidesToScroll: 3,
+        prevArrow:'<i class="fas fa-arrow-left about_slider_left"></i>',
+        nextArrow:'<i class="fas fa-arrow-right about_slider_right"></i>',
+    })
 })
